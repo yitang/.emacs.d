79d96c90ab8ab69a61ad61d0041b223c6c2b7bd5
Yi Tang <yi.tang.uk@me.com>
2014-11-07 23:33:23 +0000

    remove all files in elpa, cask

--- elpa/bookmark+-20140706.1807/bookmark+-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "bookmark+" "20140706.1807" "Bookmark+: extensions to standard library `bookmark.el'." 'nil :url "http://www.emacswiki.org/bookmark+.el" :keywords
-  '("bookmarks" "bookmark+" "placeholders" "annotations" "search" "info" "url" "w3m" "gnus"))
--- elpa/org-8.2.7c/mk/version.mk
+++
@@ -1,2 +0,0 @@
-ORGVERSION	?= 8.2.7c
-GITVERSION	?= 8.2.7c-dist
