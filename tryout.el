c430eb08c15b2d9638877275dd6bb4ac963b2e86
Yi Tang <yi.tang.uk@me.com>
2015-04-14 20:54:05 +0100

    add packages and minor changes
    voca-builder and org-download packages

--- tryout.el
+++ tryout.el
@@ -4,6 +4,5 @@
 (setq w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.")
 ;; (setq w3m-user-agent "Emacs-w3m/1.4.540 w3m/0.5.3+debian-15")
 
-(set-face-attribute 'org-done nil :strike-through t)
-(set-face-attribute 'org-headline-done nil :strike-through t)
+
 (olivetti-mode)
