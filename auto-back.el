0155d26b0a3bb46a8d66bd8a16b133998b91c2a2
Yi Tang <yi.tang.uk@me.com>
2015-05-18 17:45:51 +0100

    fix a bug

--- auto-back.el
+++ auto-back.el
@@ -6,8 +6,8 @@
     (count-matches (concat "* " keyword))))
 
 (defun org-effectiveness-count-keyword-list (keywords-list)
-   (mapcar 'org-effectiveness-count-keyword keywords-list))
-    
+  (mapcar 'org-effectiveness-count-keyword keywords-list))
+
 (defun count-keyword-file (this-file)
   (with-temp-buffer
     (insert-file-contents  this-file)
@@ -24,8 +24,8 @@
   (interactive)
   (let* ((eff-table (org-effectiveness-table))
          (eff-table-str (prin1-to-string eff-table)))
-         ;; (ymd (format-time-string "%Y-%m-%d"))
-         ;; (file-name (concat effectiveness-data-dir "/" ymd "-auto")))
+    ;; (ymd (format-time-string "%Y-%m-%d"))
+    ;; (file-name (concat effectiveness-data-dir "/" ymd "-auto")))
     (with-temp-file file-name
       (insert ymd)
       (insert "\n")
@@ -46,16 +46,10 @@ Move it to ~/git/.emacs.d/keyfreq with file name being the date and machine"
   ;; 			    "-auto"))
   ;; 	 (temporary-file-directory "~/git/.emacs.d/keyfreq")
   ;; 	 (file-name (make-temp-file (concat file-name "-")))) ;; - add ann random string to the end
-    ;; (shell-command (concat "cd ~/git/.emacs.d/keyfreq/; mv ~/.emacs.keyfreq " file-name))
+  ;; (shell-command (concat "cd ~/git/.emacs.d/keyfreq/; mv ~/.emacs.keyfreq " file-name))
   (shell-command (concat "mv ~/.emacs.keyfreq " file-name)
-    ;;    (print file-name)
-    ))
-
-(setq effectiveness-keywords (list "TODO" "NEXT" "SOMEDAY"  "DONE"  "HOLD"  "CANCELLED" "PHONE" "MEETING"))
-(setq effectiveness-data-dir "/home/yitang/git/.emacs.d/effectiveness")
-
-
-
+		 ;;    (print file-name)
+		 ))
 
 (defun yt/file-name-with-ymd (base-dir append)
   (interactive)
@@ -69,14 +63,19 @@ Move it to ~/git/.emacs.d/keyfreq with file name being the date and machine"
 	 (temporary-file-directory base-dir)
 	 (file-name (make-temp-file (concat file-name "-"))))
     file-name))
-(setq keyfreq-file (yt/file-name-with-ymd "~/git/.emacs.d/keyfreq" "auto"))
-(setq effectiveness-file (yt/file-name-with-ymd "~/git/.emacs.d/effectiveness" "auto"))
 
+(setq keyfreq-backup-file (yt/file-name-with-ymd "~/git/.emacs.d/keyfreq" "auto"))
+(setq effectiveness-backup-file (yt/file-name-with-ymd "~/git/.emacs.d/effectiveness" "auto"))
 
-(message "gonna backup")
-(message keyfreq-file)
-(message effectiveness-file)
-;; (yt/bakcup-todo-keywords-no-ask)
-;; (yt/daily-back-keyfreq-no-ask)
-(message "88")
-;; emacs --batch -l ~/.emacs -l ~/git/.emacs.d/auto-back.el -f save-buffer >% ~/tmp.emacs.batch.log
+(setq effectiveness-keywords (list "TODO" "NEXT" "SOMEDAY"  "DONE"  "HOLD"  "CANCELLED" "PHONE" "MEETING"))
+(setq effectiveness-data-dir "/home/yitang/git/.emacs.d/effectiveness")
+
+(message "going to backup")
+(message keyfreq-backup-file)
+(message effectiveness-backup-file)
+;; (yt/bakcup-todo-keywords-no-ask keyfreq-backup-file)
+;; (yt/daily-back-keyfreq-no-ask effectiveness-backup-file)
+(message "auto backup completed")
+
+;; ;; emacs --batch -l ~/.emacs -l ~/git/.emacs.d/auto-back.el -f save-buffer >% ~/tmp.emacs.batch.log
+
