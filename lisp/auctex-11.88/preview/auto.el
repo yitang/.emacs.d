aef17533bed998361a2f77f35ed27a10b2cd88d0
Yi Tang <yi.tang.uk@me.com>
2015-05-20 15:13:14 +0100

    delete lisp/ folder,
    no needed anymoe

--- lisp/auctex-11.88/preview/auto.el
+++
@@ -1,5 +0,0 @@
-
-(defvar preview-lispdir (expand-file-name "auctex" (file-name-directory load-file-name)))
-(add-to-list 'load-path preview-lispdir)
-(defvar preview-datadir (expand-file-name "auctex" (file-name-directory load-file-name)))
-(defvar preview-TeX-style-dir (expand-file-name "auctex/latex" (file-name-directory load-file-name)))
